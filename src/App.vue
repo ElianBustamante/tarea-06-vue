ecc118dd734fe82104e5341ce0a3b7ce9f253f37
Elian Bustamante <e.bustamante02@ufromail.cl>
2022-06-05 15:53:55 -0400

    Agregar estructura tarea 06 Vue

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <EditableTable
+      v-model="users"
+      :fields="fields"
+      @submit="handleUpdateUser($event)"
+      @remove="handleRemoveUser($event)"
+    ></EditableTable>
+  </div>
+</template>
+
+<script>
+import EditableTable from "./components/EditableTable.vue";
+import { addUser, deleteUser, getUsers, updateUser } from "./services/user";
+export default {
+  name: "App",
+  components: {
+    EditableTable,
+  },
+  data() {
+    return {
+      fields: [
+        { key: "selectRow", label: "", class: "select-width" },
+        { key: "name", label: "Name", type: "text", required: true },
+        {
+          key: "email",
+          label: "Email",
+          type: "email",
+          required: true,
+          pattern: ".+@bootstrapvue\\.com",
+        },
+        { key: "phone", label: "Phone", type: "text" },
+        { key: "edit", label: "", type: "edit" },
+      ],
+      users: [],
+    };
+  },
+  async mounted() {
+    this.users = await getUsers();
+  },
+  methods: {
+    async handleUpdateUser(user) {
+      if (user.id) {
+        await updateUser(user);
+      } else {
+        await addUser(user);
+      }
+    },
+    async handleRemoveUser(user) {
+      if (user.length > 0) {
+        await user.map(async (item) => {
+          await deleteUser(item.id);
+        });
+      } else {
+        await deleteUser(user.id);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  margin: 20px;
+}
+
+.select-width {
+  width: 50px;
+}
+</style>
